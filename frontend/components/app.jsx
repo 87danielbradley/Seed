175391950e4236ff984efa5bf88dbd3d81c03c19
daniel <87danielbradley@gmail.com>
2021-10-28 23:42:51 -0400

    Created starting files

---
+++ frontend/components/app.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+
+const App = () => (
+    <div>
+        <Switch>
+            <Route path="/" />
+            <Route path="/" />
+            <Route path="/" />
+        </Switch>
+    </div>
+);
+
+export default App;
---
+++ frontend/reducers/root_reducer.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux';
+
+
+const RootReducer = combineReducers(
+    {reducer1:'first reducer'}
+)
+
+export default RootReducer;
