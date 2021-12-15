73ca647498bc3ee323536247f9f99eff10e50e7c
daniel <87danielbradley@gmail.com>
2021-12-14 21:39:50 -0500

    styling update

--- frontend/components/social_links/media_view.jsx
+++ frontend/components/social_links/media_view.jsx
@@ -15,9 +15,9 @@ class SocialView extends React.Component{
             <div className="social-bar-main">
                 <div>  SeedBed, PBC © 2021  </div>
                 <div className="social-links">
-                    <a href="https://github.com/87danielbradley" ><i className="fab fa-github-square"></i></a>
-                    <a href="https://www.linkedin.com/in/87danielbradley/" ><i className="fab fa-linkedin"></i></a>
-                    <a href="https://angel.co/u/daniel-james-bradley" ><i className="fab fa-angellist"></i></a>
+                    <a href="https://github.com/87danielbradley" ><i className="fab fa-github-square fa-3x"></i></a>
+                    <a href="https://www.linkedin.com/in/87danielbradley/" ><i className="fab fa-linkedin fa-3x"></i></a>
+                    <a href="https://angel.co/u/daniel-james-bradley" ><i className="fab fa-angellist fa-3x"></i></a>
                 </div>
                
             </div>
